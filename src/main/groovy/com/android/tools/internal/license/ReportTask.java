5023adc0b92885a70da403e519a089af360d4570
Chris Warrington <cmw@google.com>
2016-06-01 10:27:15 +0000

    Revert "Cleanup: move LicenceReport plugin and task to java."
    This reverts commit e670e2230b1bc6a86b37f4db6af5cfdc26345ed8.
    
    Change-Id: I8e4b798f5d4ea42c7e0e353aa02c8b25c44f1cee

--- src/main/groovy/com/android/tools/internal/license/ReportTask.java
+++ src/main/groovy/com/android/tools/internal/license/ReportTask.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,13 +35,7 @@ import org.gradle.api.tasks.TaskAction;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Tasks outputting the license information for the external dependencies.
