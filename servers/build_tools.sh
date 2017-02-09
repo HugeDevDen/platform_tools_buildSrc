920f7c26e8c083fbef6517078e6572b4b786db08
Chris Warrington <cmw@google.com>
2017-02-09 14:10:04 +0000

    Fix makeSdk server build.
    Set --project-dir to tools/ rather than --build-file.
    See https://docs.gradle.org/current/userguide/tutorial_gradle_command_line.html#sec:selecting_build
    
    Test: ./tools/buildSrc/servers/build_tools.sh out $PWD/dist 01
    Change-Id: Ic21e7a79910bdc11f8fe894703a6317b3b26eedd

--- servers/build_tools.sh
+++ servers/build_tools.sh
@@ -51,8 +51,8 @@ cd "$PROG_DIR"
 
 GRADLE_FLAGS="--no-daemon --info"
 
-( set -x ; OUT_DIR="$OUT_DIR" DIST_DIR="$DIST_DIR" BUILD_NUMBER="$BNUM" ../../gradlew -p ../.. $GRADLE_FLAGS :sdk:eclipse:copydeps buildEclipse ) || exit $?
+( set -x ; OUT_DIR="$OUT_DIR" DIST_DIR="$DIST_DIR" BUILD_NUMBER="$BNUM" ../../gradlew --project-dir ../.. $GRADLE_FLAGS :sdk:eclipse:copydeps buildEclipse ) || exit $?
 
 # temp disable --parallel builds
 #OUT_DIR="$OUT_DIR" DIST_DIR="$DIST_DIR" ../../gradlew -b ../../build.gradle --parallel-threads="${NUM_THREADS:-47}" $GRADLE_FLAGS makeSdk
-( set -x ; OUT_DIR="$OUT_DIR" DIST_DIR="$DIST_DIR" BUILD_NUMBER="$BNUM" ../../gradlew -b ../../build.gradle $GRADLE_FLAGS $TARGET ) || exit $?
+( set -x ; OUT_DIR="$OUT_DIR" DIST_DIR="$DIST_DIR" BUILD_NUMBER="$BNUM" ../../gradlew --project-dir ../.. $GRADLE_FLAGS $TARGET ) || exit $?
